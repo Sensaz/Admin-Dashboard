5c419809d96c1defe3f22d230bc1a2d0d9be4cee
Klaudiusz  Wiktor <klaudiusz.wiktor1@gmail.com>
2023-02-26 21:02:17 +0100

    Fix dark mode in ThemeSettings component

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -4,7 +4,6 @@ import { BsCheck } from "react-icons/bs";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { themeColors } from "../data/dummy";
-
 import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
@@ -13,7 +12,7 @@ const ThemeSettings = () => {
 
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
-      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52]">
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] w-400">
         <div className="flex justify-between items-center p-4 ml-4">
           <p className="font-semibold text-lg">Settings</p>
           <button
@@ -26,7 +25,8 @@ const ThemeSettings = () => {
           </button>
         </div>
         <div className="flex-col border-t-1 border-color p-4 ml-4">
-          <p className="font-semibold text-lg">Theme Options</p>
+          <p className="font-semibold text-xl">Theme Option</p>
+
           <div className="mt-4">
             <input
               type="radio"
@@ -37,27 +37,29 @@ const ThemeSettings = () => {
               onChange={setMode}
               checked={currentMode === "Light"}
             />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="light" className="ml-2 text-md cursor-pointer">
               Light
             </label>
           </div>
-          <div className="mt-4">
+          <div className="mt-2">
             <input
               type="radio"
               id="dark"
               name="theme"
               value="Dark"
-              className="cursor-pointer"
               onChange={setMode}
+              className="cursor-pointer"
               checked={currentMode === "Dark"}
             />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
               Dark
             </label>
           </div>
         </div>
-        <div className="flex-col border-t-1 border-color p-4 ml-4">
-          <p className="font-semibold text-lg">Theme Colors</p>
+        <div className="p-4 border-t-1 border-color ml-4">
+          <p className="font-semibold text-xl">Theme Colors</p>
           <div className="flex gap-3">
             {themeColors.map((item, index) => (
               <TooltipComponent
@@ -65,17 +67,18 @@ const ThemeSettings = () => {
                 content={item.name}
                 position="TopCenter"
               >
-                <div className="relative mt-2 cursor-pointer flex hap-5 items-center">
+                <div
+                  className="relative mt-2 cursor-pointer flex gap-5 items-center"
+                  key={item.name}
+                >
                   <button
                     type="button"
                     className="h-10 w-10 rounded-full cursor-pointer"
-                    style={{
-                      backgroundColor: item.color,
-                    }}
+                    style={{ backgroundColor: item.color }}
                     onClick={() => setColor(item.color)}
                   >
                     <BsCheck
-                      className={`ml-2 text-2xl text-2hite ${
+                      className={`ml-2 text-2xl text-white ${
                         item.color === currentColor ? "block" : "hidden"
                       }`}
                     />
