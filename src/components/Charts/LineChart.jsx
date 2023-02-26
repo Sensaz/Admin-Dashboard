224be90db485bf6d5d1725f4975164c67b998a4a
Klaudiusz  Wiktor <86987965+Sensaz@users.noreply.github.com>
2023-02-26 21:05:29 +0100

    Revert "Completed Line component"
    This reverts commit 640c5e4582bae3f0ef42044eba8568891c69e060.

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,44 +1,7 @@
 import React from "react";
-import {
-  ChartComponent,
-  SeriesCollectionDirective,
-  Seresdirective,
-  Inject,
-  LineSeries,
-  DateTime,
-  Legend,
-  Tooltip,
-  SeriesDirective,
-} from "@syncfusion/ej2-react-charts";
-
-import {
-  lineCustomSeries,
-  LinePrimaryYAxis,
-  LinePrimaryXAxis,
-} from "../../data/dummy";
-
-import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
-  const { currentMode } = useStateContext();
-  return (
-    <ChartComponent
-      id="line-chart"
-      height="420px"
-      primaryXAxis={LinePrimaryXAxis}
-      primaryYAxis={LinePrimaryYAxis}
-      chartArea={{ border: { width: 0 } }}
-      tooltip={{ enable: true }}
-      background={currentMode === "Dark" ? "#33373E" : "#fff"}
-    >
-      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
-      <SeriesCollectionDirective>
-        {lineCustomSeries.map((item, index) => (
-          <SeriesDirective key={index} {...item} />
-        ))}
-      </SeriesCollectionDirective>
-    </ChartComponent>
-  );
+  return <div>LineChart</div>;
 };
 
 export default LineChart;
